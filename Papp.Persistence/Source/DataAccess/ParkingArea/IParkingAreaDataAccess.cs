56e02995f0f79e954a44c6268bee4cda3e48c475
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-09-20 14:28:53 +0200

    [PAPP-683] Implemented ParkingArea DA

---
+++ Papp.Persistence/Source/DataAccess/ParkingArea/IParkingAreaDataAccess.cs
@@ -0,0 +1,16 @@
+using Papp.Domain;
+
+namespace Papp.Persistence.DataAccess;
+
+/// <summary>
+/// Interface defining specific methods for the ParkingArea domain object.
+/// </summary>
+public interface IParkingAreaDataAccess : IGenericDataAccess<ParkingArea>
+{
+    /// <summary>
+    /// Check if a ParkingArea exists in the DB by the given id.
+    /// </summary>
+    /// <param name="id">An id to check for.</param>
+    /// <returns>Whether or not a matching id ParkingArea could be found.</returns>
+    Task<bool> Exists(int id);
+}
