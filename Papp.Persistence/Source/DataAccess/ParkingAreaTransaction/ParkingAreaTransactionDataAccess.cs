66939b585de1f3c1ac80241b936dfbcc85ae50d3
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-09-21 13:44:36 +0200

    [PAPP-684] Implemented ParkingAreaTransaction DA

---
+++ Papp.Persistence/Source/DataAccess/ParkingAreaTransaction/ParkingAreaTransactionDataAccess.cs
@@ -0,0 +1,21 @@
+using Papp.Domain;
+
+namespace Papp.Persistence.DataAccess;
+
+/// <inheritdoc/>
+public class ParkingAreaTransactionDataAccess : GenericDataAccess<ParkingAreaTransaction>, IParkingAreaTransactionDataAccess
+{
+    private readonly PappDbContext context;
+
+    public ParkingAreaTransactionDataAccess(PappDbContext context) : base(context)
+    {
+        this.context = context;
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> Exists(Guid id)
+    {
+        var entity = await base.GetFirstOrDefaultAsync(e => e.Id.Equals(id));
+        return entity != null;
+    }
+}
