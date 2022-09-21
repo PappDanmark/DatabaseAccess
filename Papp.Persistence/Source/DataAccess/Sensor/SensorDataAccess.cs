29bf03c93af1dd2c3fc95268fe764c9094c0acba
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-09-21 15:47:46 +0200

    [PAPP-686] Implemented Sensor DA

---
+++ Papp.Persistence/Source/DataAccess/Sensor/SensorDataAccess.cs
@@ -0,0 +1,21 @@
+using Papp.Domain;
+
+namespace Papp.Persistence.DataAccess;
+
+/// <inheritdoc/>
+public class SensorDataAccess : GenericDataAccess<Sensor>, ISensorDataAccess
+{
+    private readonly PappDbContext context;
+
+    public SensorDataAccess(PappDbContext context) : base(context)
+    {
+        this.context = context;
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> Exists(string id)
+    {
+        var entity = await base.GetFirstOrDefaultAsync(e => e.Id.Equals(id));
+        return entity != null;
+    }
+}
