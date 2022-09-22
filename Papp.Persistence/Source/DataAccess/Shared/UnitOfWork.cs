cb723cbce11eb0a69da1dab96a483f581b1498be
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-09-22 14:53:23 +0200

    [PAPP-728] Simplified the names of UnitOfWork class to match the names of PappContext tables

--- Papp.Persistence/Source/DataAccess/Shared/UnitOfWork.cs
+++ Papp.Persistence/Source/DataAccess/Shared/UnitOfWork.cs
@@ -3,44 +3,44 @@ namespace Papp.Persistence.DataAccess;
 public class UnitOfWork : IUnitOfWork
 {
     private PappDbContext context;
-    public IBoothDataAccess BoothDataAccess { get; private set; }
-    public IBundleDataAccess BundleDataAccess{ get; private set; }
-    public IChargerDataAccess ChargerDataAccess{ get; private set; }
-    public IChargerConnectorDataAccess ChargerConnectorDataAccess{ get; private set; }
-    public IChargerTypeDataAccess ChargerTypeDataAccess{ get; private set; }
-    public ICountryDataAccess CountryDataAccess{ get; private set; }
-    public IImageDataAccess ImageDataAccess{ get; private set; }
-    public IManufacturerDataAccess ManufacturerDataAccess{ get; private set; }
-    public IOperatorDataAccess OperatorDataAccess{ get; private set; }
-    public IParkingAreaDataAccess ParkingAreaDataAccess{ get; private set; }
-    public IParkingAreaTransactionDataAccess ParkingAreaTransactionDataAccess{ get; private set; }
-    public IParkingBoothDataAccess ParkingBoothDataAccess{ get; private set; }
-    public ISensorDataAccess SensorDataAccess{ get; private set; }
-    public ISensorBatteryUpdateDataAccess SensorBatteryUpdateDataAccess{ get; private set; }
-    public ISensorInstallDataAccess SensorInstallDataAccess{ get; private set; }
-    public ISensorTypeDataAccess SensorTypeDataAccess{ get; private set; }
-    public ISensorUpdateDataAccess SensorUpdateDataAccess{ get; private set; }
+    public IBoothDataAccess Booths { get; private set; }
+    public IBundleDataAccess Bundles { get; private set; }
+    public IChargerDataAccess Chargers { get; private set; }
+    public IChargerConnectorDataAccess ChargerConnectors { get; private set; }
+    public IChargerTypeDataAccess ChargerTypes { get; private set; }
+    public ICountryDataAccess Countries { get; private set; }
+    public IImageDataAccess Images { get; private set; }
+    public IManufacturerDataAccess Manufacturers { get; private set; }
+    public IOperatorDataAccess Operators { get; private set; }
+    public IParkingAreaDataAccess ParkingAreas { get; private set; }
+    public IParkingAreaTransactionDataAccess ParkingAreaTransactions { get; private set; }
+    public IParkingBoothDataAccess ParkingBooths { get; private set; }
+    public ISensorDataAccess Sensors { get; private set; }
+    public ISensorBatteryUpdateDataAccess SensorBatteryUpdates { get; private set; }
+    public ISensorInstallDataAccess SensorInstalls { get; private set; }
+    public ISensorTypeDataAccess SensorTypes { get; private set; }
+    public ISensorUpdateDataAccess SensorUpdates { get; private set; }
 
     public UnitOfWork(PappDbContext context)
     {
         this.context = context;
-        this.BoothDataAccess = new BoothDataAccess(context);
-        this.BundleDataAccess = new BundleDataAccess(context);
-        this.ChargerDataAccess = new ChargerDataAccess(context);
-        this.ChargerConnectorDataAccess = new ChargerConnectorDataAccess(context);
-        this.ChargerTypeDataAccess = new ChargerTypeDataAccess(context);
-        this.CountryDataAccess = new CountryDataAccess(context);
-        this.ImageDataAccess = new ImageDataAccess(context);
-        this.ManufacturerDataAccess = new ManufacturerDataAccess(context);
-        this.OperatorDataAccess = new OperatorDataAccess(context);
-        this.ParkingAreaDataAccess = new ParkingAreaDataAccess(context);
-        this.ParkingAreaTransactionDataAccess = new ParkingAreaTransactionDataAccess(context);
-        this.ParkingBoothDataAccess = new ParkingBoothDataAccess(context);
-        this.SensorDataAccess = new SensorDataAccess(context);
-        this.SensorBatteryUpdateDataAccess = new SensorBatteryUpdateDataAccess(context);
-        this.SensorInstallDataAccess = new SensorInstallDataAccess(context);
-        this.SensorTypeDataAccess = new SensorTypeDataAccess(context);
-        this.SensorUpdateDataAccess = new SensorUpdateDataAccess(context);
+        this.Booths = new BoothDataAccess(context);
+        this.Bundles = new BundleDataAccess(context);
+        this.Chargers = new ChargerDataAccess(context);
+        this.ChargerConnectors = new ChargerConnectorDataAccess(context);
+        this.ChargerTypes = new ChargerTypeDataAccess(context);
+        this.Countries = new CountryDataAccess(context);
+        this.Images = new ImageDataAccess(context);
+        this.Manufacturers = new ManufacturerDataAccess(context);
+        this.Operators = new OperatorDataAccess(context);
+        this.ParkingAreas = new ParkingAreaDataAccess(context);
+        this.ParkingAreaTransactions = new ParkingAreaTransactionDataAccess(context);
+        this.ParkingBooths = new ParkingBoothDataAccess(context);
+        this.Sensors = new SensorDataAccess(context);
+        this.SensorBatteryUpdates = new SensorBatteryUpdateDataAccess(context);
+        this.SensorInstalls = new SensorInstallDataAccess(context);
+        this.SensorTypes = new SensorTypeDataAccess(context);
+        this.SensorUpdates = new SensorUpdateDataAccess(context);
     }
 
     public async Task SaveChangesAsync()
