ef51d95001ff682a3c6497bceab9717399f63a6a
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-04 14:26:18 +0200

    [PAPP-736] Added ordering functionality to the specification pattern

--- Papp.Persistence/Source/DataAccess/Shared/BaseSpecification.cs
+++ Papp.Persistence/Source/DataAccess/Shared/BaseSpecification.cs
@@ -9,12 +9,19 @@ public abstract class BaseSpecification<TEntity> : IBaseSpecification<TEntity>
     public List<string> IncludeStrings { get; }
     public bool Tracked { get; }
 
+    public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
+    public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
+    public Expression<Func<TEntity, object>>? GroupBy { get; private set; }
+
     public BaseSpecification(Expression<Func<TEntity, bool>> criteria, bool tracked = false)
     {
         this.Criteria = criteria;
         this.IncludeExpressions = new();
         this.IncludeStrings = new();
         this.Tracked = tracked;
+        this.OrderBy = null;
+        this.OrderByDescending = null;
+        this.GroupBy = null;
     }
 
     protected virtual void AddInclude(Expression<Func<TEntity, object>> expression)
@@ -25,5 +32,20 @@ public abstract class BaseSpecification<TEntity> : IBaseSpecification<TEntity>
     protected virtual void AddInclude(string includeString)
     {
         this.IncludeStrings.Add(includeString);
+    }
+
+    protected virtual void ApplyOrderBy(Expression<Func<TEntity, object>> expression)
+    {
+        OrderBy = expression;
+    }
+
+    protected virtual void ApplyOrderByDescending(Expression<Func<TEntity, object>> expression)
+    {
+        OrderByDescending = expression;
+    }
+
+    protected virtual void ApplyGroupBy(Expression<Func<TEntity, object>> expression)
+    {
+        GroupBy = expression;
     }
 }
--- Papp.Persistence/Source/DataAccess/Shared/SpecificationEvaluator.cs
+++ Papp.Persistence/Source/DataAccess/Shared/SpecificationEvaluator.cs
@@ -18,7 +18,22 @@ public class SpecificationEvaluator<TEntity> where TEntity : class
         query = specification.IncludeExpressions.Aggregate(query, (current, include) => current.Include(include));
 
         query = specification.IncludeStrings.Aggregate(query, (current, include) => current.Include(include));
-        
+
+        // Apply ordering if expressions are set.
+        if (specification.OrderBy != null)
+        {
+            query = query.OrderBy(specification.OrderBy);
+        }
+        else if (specification.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(specification.OrderByDescending);
+        }
+
+        if (specification.GroupBy != null)
+        {
+            query = query.GroupBy(specification.GroupBy).SelectMany(x => x);
+        }
+
         // To debug/print the sql queries that get executed on the db.
         // Console.WriteLine(query.ToQueryString());
 
