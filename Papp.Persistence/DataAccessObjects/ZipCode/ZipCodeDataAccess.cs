4ff111a3a15472bb3180263d122577351571be64
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-10 22:20:08 +0200

    [PAPP-741] Moved out shared folder

--- Papp.Persistence/DataAccessObjects/ZipCode/ZipCodeDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/ZipCode/ZipCodeDataAccess.cs
@@ -5,11 +5,16 @@ namespace Papp.Persistence.DataAccess;
 /// <inheritdoc/>
 public class ZipCodeDataAccess : GenericDataAccess<ZipCode>, IZipCodeDataAccess
 {
-    private readonly PappDbContext context;
+    private readonly PappDbContext DbContext;
 
     public ZipCodeDataAccess(PappDbContext context) : base(context)
     {
-        this.context = context;
+        this.DbContext = context;
+    }
+
+    public ZipCodeDataAccess(IUnitOfWork<PappDbContext> unitOfWork): base(unitOfWork)
+    {
+        this.DbContext = unitOfWork.DbContext;
     }
 
     /// <inheritdoc/>
