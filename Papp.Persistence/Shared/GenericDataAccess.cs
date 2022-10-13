2ccfd15f5d507ca4773ee79d844369ef0d6ef941
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-13 11:24:47 +0200

    [PAPP-742] Added extensive, modular Create and Read methods for generic data acess class

--- Papp.Persistence/Shared/GenericDataAccess.cs
+++ Papp.Persistence/Shared/GenericDataAccess.cs
@@ -1,38 +1,191 @@
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace Papp.Persistence.DataAccess;
 
-/// <inheritdoc/>
-public class GenericDataAccess<T> : IGenericDataAccess<T> where T : class
+public class GenericDataAccess<TEntity> : IGenericDataAccess<TEntity> where TEntity : class
 {
     private readonly DbContext DbContext;
-    internal DbSet<T> DbSet;
+    private readonly DbSet<TEntity> DbSet;
 
     public GenericDataAccess(DbContext context)
     {
         this.DbContext = context;
-        this.DbSet = context.Set<T>();
+        this.DbSet = context.Set<TEntity>();
     }
 
     public GenericDataAccess(IUnitOfWork<DbContext> unitOfWork) : this(unitOfWork.DbContext)
     {
     }
 
-    /// <inheritdoc/>
-    public async Task AddAsync(T entity)
+    #region Read
+
+    private IQueryable<TEntity> GetDbSetQueryable(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
     {
-        await DbSet.AddAsync(entity);
+        var query = tracking ? this.DbSet : this.DbSet.AsNoTracking();
+
+        if (include != null)
+        {
+            query = include(query);
+        }
+        if (predicate != null)
+        {
+            query = query.Where(predicate);
+        }
+        if (orderBy != null)
+        {
+            query = orderBy(query);
+        }
+
+        return query;
     }
 
     /// <inheritdoc/>
-    public async Task<IList<T>> GetAllAsync(IBaseSpecification<T>? specification = null)
+    public virtual TEntity? GetFirstOrDefault(
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
     {
-        return await SpecificationEvaluator<T>.GetQuery(DbContext.Set<T>(), specification).ToListAsync();
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).FirstOrDefault();
     }
 
     /// <inheritdoc/>
-    public async Task<T?> GetFirstOrDefaultAsync(IBaseSpecification<T> specification)
+    public virtual TResult? GetFirstOrDefault<TResult>(
+        Expression<Func<TEntity, TResult>> selector,
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
     {
-        return await SpecificationEvaluator<T>.GetQuery(DbContext.Set<T>(), specification).FirstOrDefaultAsync();
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).Select(selector).FirstOrDefault();
     }
+
+    /// <inheritdoc/>
+    public virtual async Task<TEntity?> GetFirstOrDefaultAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return await this.GetDbSetQueryable(predicate, orderBy, include, tracking).FirstOrDefaultAsync();
+    }
+
+    /// <inheritdoc/>
+    public virtual async Task<TResult?> GetFirstOrDefaultAsync<TResult>(
+        Expression<Func<TEntity, TResult>> selector,
+        Expression<Func<TEntity, bool>> predicate,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return await this.GetDbSetQueryable(predicate, orderBy, include, tracking).Select(selector).FirstOrDefaultAsync();
+    }
+
+    /// <inheritdoc/>
+    public virtual IEnumerable<TEntity> GetAllAsEnumerable(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).AsEnumerable();
+    }
+
+    /// <inheritdoc/>
+    public virtual IEnumerable<TResult> GetAllAsEnumerable<TResult>(
+        Expression<Func<TEntity, TResult>> selector,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).Select(selector).AsEnumerable();
+    }
+
+    /// <inheritdoc/>
+    public virtual IAsyncEnumerable<TEntity> GetAllAsAsyncEnumerable(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).AsAsyncEnumerable();
+    }
+
+    /// <inheritdoc/>
+    public virtual IAsyncEnumerable<TResult> GetAllAsAsyncEnumerable<TResult>(
+        Expression<Func<TEntity, TResult>> selector,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).Select(selector).AsAsyncEnumerable();
+    }
+
+    /// <inheritdoc/>
+    public virtual ICollection<TEntity> GetAllAsCollection(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).ToList();
+    }
+
+    /// <inheritdoc/>
+    public virtual ICollection<TResult> GetAllAsCollection<TResult>(
+        Expression<Func<TEntity, TResult>> selector,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return this.GetDbSetQueryable(predicate, orderBy, include, tracking).Select(selector).ToList();
+    }
+
+    /// <inheritdoc/>
+    public virtual async Task<ICollection<TEntity>> GetAllAsCollectionAsync(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return await this.GetDbSetQueryable(predicate, orderBy, include, tracking).ToListAsync();
+    }
+
+    /// <inheritdoc/>
+    public virtual async Task<ICollection<TResult>> GetAllAsCollectionAsync<TResult>(
+        Expression<Func<TEntity, TResult>> selector,
+        Expression<Func<TEntity, bool>>? predicate = null,
+        Func<IQueryable<TEntity>?, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>?, IIncludableQueryable<TEntity, object>>? include = null,
+        bool tracking = false)
+    {
+        return await this.GetDbSetQueryable(predicate, orderBy, include, tracking).Select(selector).ToListAsync();
+    }
+
+    #endregion
+
+    #region Add
+
+    /// <inheritdoc/>
+    public void Add(TEntity entity)
+    {
+        this.DbSet.Add(entity);
+    }
+
+    /// <inheritdoc/>
+    public async Task AddAsync(TEntity entity)
+    {
+        await this.DbSet.AddAsync(entity);
+    }
+
+    #endregion
 }
