c6f1630b6a02ce1fd3a15cefe14a5be5ceaeb351
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-10-27 13:05:49 +0200

    [PAPP-760] Moved the method to get the updates of a booth into the data access layer

--- Papp.Persistence/DataAccessObjects/SensorUpdate/ISensorUpdateDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/SensorUpdate/ISensorUpdateDataAccess.cs
@@ -12,4 +12,12 @@ public interface ISensorUpdateDataAccess : IGenericDataAccess<SensorUpdate>
     /// </summary>
     /// <returns>Latest Sensor Update.</returns>
     SensorUpdate GetLastestBySensorId(string id);
+
+    /// <summary>
+    /// Retrieves all Sensor Updates for a given Booth id, since a specific point in time till present.
+    /// </summary>
+    /// <param name="boothId">The id of the Booth from which to get the updates.</param>
+    /// <param name="timestamp">The point of time since when to retrieve the updates.</param>
+    /// <returns>A list of mathching Sensor Updates.</returns>
+    IEnumerable<SensorUpdate> GetAllByBoothIdSince(Guid boothId, DateTime timestamp);
 }
