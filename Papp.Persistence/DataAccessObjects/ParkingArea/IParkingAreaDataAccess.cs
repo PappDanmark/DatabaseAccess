1c8cb1febb6b39e49fa93e4d0d9205579c725480
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-07 13:26:39 +0100

    [PAPP-767] Implemented update method for Parking Area data access

--- Papp.Persistence/DataAccessObjects/ParkingArea/IParkingAreaDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/ParkingArea/IParkingAreaDataAccess.cs
@@ -7,4 +7,21 @@ namespace Papp.Persistence.DataAccess;
 /// </summary>
 public interface IParkingAreaDataAccess : IGenericDataAccess<ParkingArea>
 {
+    /// <summary>
+    /// Updates a ParkingArea entity using the specified id.
+    /// </summary>
+    /// <param name="id">The id of the ParkingArea which needs to be updated.</param>
+    /// <param name="parkingArea">The new vesion of the ParkingArea used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified ParkingArea entity, or null if a ParkingArea for the provided id doesn't exist.</returns>
+    ParkingArea? Update(int id, ParkingArea parkingArea);
+
+    /// <summary>
+    /// Updates asynchronously a ParkingArea entity using the specified id.
+    /// </summary>
+    /// <param name="id">The id of the ParkingArea which needs to be updated.</param>
+    /// <param name="parkingArea">The new vesion of the ParkingArea used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified ParkingArea entity, or null if a ParkingArea for the provided id doesn't exist.</returns>
+    Task<ParkingArea?> UpdateAsync(int id, ParkingArea parkingArea);
 }
--- Papp.Persistence/DataAccessObjects/ParkingArea/ParkingAreaDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/ParkingArea/ParkingAreaDataAccess.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Papp.Domain;
 using Papp.Persistence.Context;
 
@@ -16,5 +17,53 @@ public class ParkingAreaDataAccess : GenericDataAccess<ParkingArea>, IParkingAre
     public ParkingAreaDataAccess(IUnitOfWork<PappDbContext> unitOfWork): base(unitOfWork)
     {
         this.DbContext = unitOfWork.DbContext;
+    }
+
+    /// <inheritdoc/>
+    public ParkingArea? Update(int id, ParkingArea parkingArea)
+    {
+        var existing = DbContext.ParkingAreas.FirstOrDefault(e => e.Id == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.TotalSpaces = parkingArea.TotalSpaces;
+        existing.Coordinates = parkingArea.Coordinates;
+        existing.ZipCodeId = parkingArea.ZipCodeId;
+        existing.SensorTypeId = parkingArea.SensorTypeId;
+        existing.PappId = parkingArea.PappId;
+        existing.CoordinatesEntry = parkingArea.CoordinatesEntry;
+        existing.Street = parkingArea.Street;
+        existing.Name = parkingArea.Name;
+        existing.LatestOccupiedSpaces = parkingArea.LatestOccupiedSpaces;
+        existing.LatestOccupiedTimestamp = parkingArea.LatestOccupiedTimestamp;
+
+        return existing;
+    }
+
+    /// <inheritdoc/>
+    public async Task<ParkingArea?> UpdateAsync(int id, ParkingArea parkingArea)
+    {
+        var existing = await DbContext.ParkingAreas.FirstOrDefaultAsync(e => e.Id == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.TotalSpaces = parkingArea.TotalSpaces;
+        existing.Coordinates = parkingArea.Coordinates;
+        existing.ZipCodeId = parkingArea.ZipCodeId;
+        existing.SensorTypeId = parkingArea.SensorTypeId;
+        existing.PappId = parkingArea.PappId;
+        existing.CoordinatesEntry = parkingArea.CoordinatesEntry;
+        existing.Street = parkingArea.Street;
+        existing.Name = parkingArea.Name;
+        existing.LatestOccupiedSpaces = parkingArea.LatestOccupiedSpaces;
+        existing.LatestOccupiedTimestamp = parkingArea.LatestOccupiedTimestamp;
+
+        return existing;
     }
 }
