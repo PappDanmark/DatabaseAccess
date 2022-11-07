0c521bf479a83575c7872d38955ad5efad92f40c
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-07 14:10:23 +0100

    [PAPP-767] Added an update method to sensor data access

--- Papp.Persistence/DataAccessObjects/Sensor/ISensorDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Sensor/ISensorDataAccess.cs
@@ -7,4 +7,21 @@ namespace Papp.Persistence.DataAccess;
 /// </summary>
 public interface ISensorDataAccess : IGenericDataAccess<Sensor>
 {
+    /// <summary>
+    /// Updates a Sensor entity using the specified id.
+    /// </summary>
+    /// <param name="id">The id of the Sensor which needs to be updated.</param>
+    /// <param name="operator">The new vesion of the Sensor used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified Sensor entity, or null if a Sensor for the provided id doesn't exist.</returns>
+    Sensor? Update(string id, Sensor sensor);
+
+    /// <summary>
+    /// Updates asynchronously a Sensor entity using the specified id.
+    /// </summary>
+    /// <param name="id">The id of the Sensor which needs to be updated.</param>
+    /// <param name="operator">The new vesion of the Sensor used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified Sensor entity, or null if a Sensor for the provided id doesn't exist.</returns>
+    Task<Sensor?> UpdateAsync(string id, Sensor sensor);
 }
--- Papp.Persistence/DataAccessObjects/Sensor/SensorDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Sensor/SensorDataAccess.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Papp.Domain;
 using Papp.Persistence.Context;
 
@@ -16,5 +17,49 @@ public class SensorDataAccess : GenericDataAccess<Sensor>, ISensorDataAccess
     public SensorDataAccess(IUnitOfWork<PappDbContext> unitOfWork): base(unitOfWork)
     {
         this.DbContext = unitOfWork.DbContext;
+    }
+
+    /// <inheritdoc/>
+    public Sensor? Update(string id, Sensor sensor)
+    {
+        var existing = DbContext.Sensors.FirstOrDefault(e => e.SensorId == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.InstallationTimestamp = sensor.InstallationTimestamp;
+        existing.Battery = sensor.Battery;
+        existing.Occupied = sensor.Occupied;
+        existing.LastUpdatedBySensorAction = sensor.LastUpdatedBySensorAction;
+        existing.LastUpdatedTimestamp = sensor.LastUpdatedTimestamp;
+        existing.InstalledAtParkingBoothId = sensor.InstalledAtParkingBoothId;
+        existing.InstallationDateTimeEpoch = sensor.InstallationDateTimeEpoch;
+        existing.SensorTypeId = sensor.SensorTypeId;
+
+        return existing;
+    }
+
+    /// <inheritdoc/>
+    public async Task<Sensor?> UpdateAsync(string id, Sensor sensor)
+    {
+        var existing = await DbContext.Sensors.FirstOrDefaultAsync(e => e.SensorId == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.InstallationTimestamp = sensor.InstallationTimestamp;
+        existing.Battery = sensor.Battery;
+        existing.Occupied = sensor.Occupied;
+        existing.LastUpdatedBySensorAction = sensor.LastUpdatedBySensorAction;
+        existing.LastUpdatedTimestamp = sensor.LastUpdatedTimestamp;
+        existing.InstalledAtParkingBoothId = sensor.InstalledAtParkingBoothId;
+        existing.InstallationDateTimeEpoch = sensor.InstallationDateTimeEpoch;
+        existing.SensorTypeId = sensor.SensorTypeId;
+
+        return existing;
     }
 }
