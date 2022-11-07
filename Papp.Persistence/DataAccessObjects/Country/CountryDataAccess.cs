ac0d0a7e351e97cfc39426cdf77c7ef6582ea082
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-07 12:55:21 +0100

    [PAPP-767] Implemented update functionality for Country data access

--- Papp.Persistence/DataAccessObjects/Country/CountryDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Country/CountryDataAccess.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Papp.Domain;
 using Papp.Persistence.Context;
 
@@ -16,5 +17,45 @@ public class CountryDataAccess : GenericDataAccess<Country>, ICountryDataAccess
     public CountryDataAccess(IUnitOfWork<PappDbContext> unitOfWork): base(unitOfWork)
     {
         this.DbContext = unitOfWork.DbContext;
+    }
+
+    /// <inheritdoc/>
+    public Country? Update(short id, Country country)
+    {
+        var existing = DbContext.Countries.FirstOrDefault(e => e.Iso3166Numeric == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.CommonName = country.CommonName;
+        existing.OfficialName = country.OfficialName;
+        existing.Iso3166Alpha2 = country.Iso3166Alpha2;
+        existing.Iso3166Alpha3 = country.Iso3166Alpha3;
+        existing.Population = country.Population;
+        existing.AreaKm2 = country.AreaKm2;
+
+        return existing;
+    }
+
+    /// <inheritdoc/>
+    public async Task<Country?> UpdateAsync(short id, Country country)
+    {
+        var existing = await DbContext.Countries.FirstOrDefaultAsync(e => e.Iso3166Numeric == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.CommonName = country.CommonName;
+        existing.OfficialName = country.OfficialName;
+        existing.Iso3166Alpha2 = country.Iso3166Alpha2;
+        existing.Iso3166Alpha3 = country.Iso3166Alpha3;
+        existing.Population = country.Population;
+        existing.AreaKm2 = country.AreaKm2;
+
+        return existing;
     }
 }
--- Papp.Persistence/DataAccessObjects/Country/ICountryDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Country/ICountryDataAccess.cs
@@ -7,4 +7,21 @@ namespace Papp.Persistence.DataAccess;
 /// </summary>
 public interface ICountryDataAccess : IGenericDataAccess<Country>
 {
+    /// <summary>
+    /// Updates a Country entity using the specified id.
+    /// </summary>
+    /// <param name="id">The Iso3166Numeric id of the Country which needs to be updated.</param>
+    /// <param name="country">The new vesion of the Country used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified Country entity, or null if a Country for the provided id doesn't exist.</returns>
+    Country? Update(short id, Country country);
+
+    /// <summary>
+    /// Updates asynchronously a Country entity using the specified id.
+    /// </summary>
+    /// <param name="id">The Iso3166Numeric id of the Country which needs to be updated.</param>
+    /// <param name="country">The new vesion of the Country used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified Country entity, or null if a Country for the provided id doesn't exist.</returns>
+    Task<Country?> UpdateAsync(short id, Country country);
 }
