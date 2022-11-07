46e2a8f38cb8c7c006279023a0329cc338327c5c
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-07 12:15:47 +0100

    [PAPP-767] Implemented an update method for Charger model

--- Papp.Persistence/DataAccessObjects/Charger/ChargerDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Charger/ChargerDataAccess.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Papp.Domain;
 using Papp.Persistence.Context;
 
@@ -16,5 +17,37 @@ public class ChargerDataAccess : GenericDataAccess<Charger>, IChargerDataAccess
     public ChargerDataAccess(IUnitOfWork<PappDbContext> unitOfWork): base(unitOfWork)
     {
         this.DbContext = unitOfWork.DbContext;
+    }
+
+    /// <inheritdoc/>
+    public Charger? Update(Guid id, Charger charger)
+    {
+        var existing = DbContext.Chargers.FirstOrDefault(e => e.Id == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.OperatorId = charger.OperatorId;
+        existing.ChargerType = charger.ChargerType;
+
+        return existing;
+    }
+
+    /// <inheritdoc/>
+    public async Task<Charger?> UpdateAsync(Guid id, Charger charger)
+    {
+        var existing = await DbContext.Chargers.FirstOrDefaultAsync(e => e.Id == id);
+
+        if (existing == null)
+        {
+            return null;
+        }
+
+        existing.OperatorId = charger.OperatorId;
+        existing.ChargerType = charger.ChargerType;
+
+        return existing;
     }
 }
--- Papp.Persistence/DataAccessObjects/Charger/IChargerDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Charger/IChargerDataAccess.cs
@@ -7,4 +7,21 @@ namespace Papp.Persistence.DataAccess;
 /// </summary>
 public interface IChargerDataAccess : IGenericDataAccess<Charger>
 {
+    /// <summary>
+    /// Updates a Charger entity using the specified id.
+    /// </summary>
+    /// <param name="id">The id of the Charger which needs to be updates.</param>
+    /// <param name="charger">The new vesion of the Charger used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified Charger entity, or null if the a Charger for the provided id doesn't exist.</returns>
+    Charger? Update(Guid id, Charger charger);
+
+    /// <summary>
+    /// Updates asynchronously a Charger entity using the specified id.
+    /// </summary>
+    /// <param name="id">The id of the Charger which needs to be updates.</param>
+    /// <param name="charger">The new vesion of the Charger used to update the information.</param>
+    /// <remarks>Doesn't update the Id field.</remarks>
+    /// <returns>The new modified Charger entity, or null if the a Charger for the provided id doesn't exist.</returns>
+    Task<Charger?> UpdateAsync(Guid id, Charger charger);
 }
