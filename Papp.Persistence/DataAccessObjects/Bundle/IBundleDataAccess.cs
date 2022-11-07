08d0ee190abc4495b00df679c8ea4376182eaa35
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2022-11-07 12:31:54 +0100

    [PAPP-767] Fixed a few typos

--- Papp.Persistence/DataAccessObjects/Bundle/IBundleDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Bundle/IBundleDataAccess.cs
@@ -10,18 +10,18 @@ public interface IBundleDataAccess : IGenericDataAccess<Bundle>
     /// <summary>
     /// Updates a Bundle entity using the specified id.
     /// </summary>
-    /// <param name="id">The id of the Bundle which needs to be updates.</param>
+    /// <param name="id">The id of the Bundle which needs to be updated.</param>
     /// <param name="bundle">The new vesion of the Bundle used to update the information.</param>
     /// <remarks>Doesn't update the Id field.</remarks>
-    /// <returns>The new modified Bundle entity, or null if the a Bundle for the provided id doesn't exist.</returns>
+    /// <returns>The new modified Bundle entity, or null if a Bundle for the provided id doesn't exist.</returns>
     Bundle? Update(int id, Bundle bundle);
 
     /// <summary>
     /// Updates asynchronously a Bundle entity using the specified id.
     /// </summary>
-    /// <param name="id">The id of the Bundle which needs to be updates.</param>
+    /// <param name="id">The id of the Bundle which needs to be updated.</param>
     /// <param name="bundle">The new vesion of the Bundle used to update the information.</param>
     /// <remarks>Doesn't update the Id field.</remarks>
-    /// <returns>The new modified Bundle entity, or null if the a Bundle for the provided id doesn't exist.</returns>
+    /// <returns>The new modified Bundle entity, or null if a Bundle for the provided id doesn't exist.</returns>
     Task<Bundle?> UpdateAsync(int id, Bundle bundle);
 }
--- Papp.Persistence/DataAccessObjects/Charger/IChargerDataAccess.cs
+++ Papp.Persistence/DataAccessObjects/Charger/IChargerDataAccess.cs
@@ -10,18 +10,18 @@ public interface IChargerDataAccess : IGenericDataAccess<Charger>
     /// <summary>
     /// Updates a Charger entity using the specified id.
     /// </summary>
-    /// <param name="id">The id of the Charger which needs to be updates.</param>
+    /// <param name="id">The id of the Charger which needs to be updated.</param>
     /// <param name="charger">The new vesion of the Charger used to update the information.</param>
     /// <remarks>Doesn't update the Id field.</remarks>
-    /// <returns>The new modified Charger entity, or null if the a Charger for the provided id doesn't exist.</returns>
+    /// <returns>The new modified Charger entity, or null if a Charger for the provided id doesn't exist.</returns>
     Charger? Update(Guid id, Charger charger);
 
     /// <summary>
     /// Updates asynchronously a Charger entity using the specified id.
     /// </summary>
-    /// <param name="id">The id of the Charger which needs to be updates.</param>
+    /// <param name="id">The id of the Charger which needs to be updated.</param>
     /// <param name="charger">The new vesion of the Charger used to update the information.</param>
     /// <remarks>Doesn't update the Id field.</remarks>
-    /// <returns>The new modified Charger entity, or null if the a Charger for the provided id doesn't exist.</returns>
+    /// <returns>The new modified Charger entity, or null if a Charger for the provided id doesn't exist.</returns>
     Task<Charger?> UpdateAsync(Guid id, Charger charger);
 }
