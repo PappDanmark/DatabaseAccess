e7d4b02e12a1c76739c9bf76051ce8e4d325614a
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2023-01-09 17:54:14 +0100

    [PAPP-842] Created data access object for bundle

---
+++ Papp.Persistence/DataAccessObjects/ParkingBundle/IParkingBundleDataAccess.cs
@@ -0,0 +1,10 @@
+using Papp.Domain;
+
+namespace Papp.Persistence.DataAccess;
+
+/// <summary>
+/// Interface defining specific methods for the ParkingBundle domain object.
+/// </summary>
+public interface IParkingBundleDataAccess : IGenericDataAccess<ParkingBundle>
+{
+}
