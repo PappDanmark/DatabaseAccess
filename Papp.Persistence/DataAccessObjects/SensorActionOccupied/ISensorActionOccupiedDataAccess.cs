fc5f6c7cb8bdd8e1617b24eb5b2bfab0e1ab49ad
Maxim-Zavidei <maxim-zavidei+git@pm.me>
2023-01-09 17:54:34 +0100

    [PAPP-842] Created data access object for sensor oction occupied

---
+++ Papp.Persistence/DataAccessObjects/SensorActionOccupied/ISensorActionOccupiedDataAccess.cs
@@ -0,0 +1,10 @@
+using Papp.Domain;
+
+namespace Papp.Persistence.DataAccess;
+
+/// <summary>
+/// Interface defining specific methods for the SensorActionOccupied domain object.
+/// </summary>
+public interface ISensorActionOccupiedDataAccess : IGenericDataAccess<SensorActionOccupied>
+{
+}
